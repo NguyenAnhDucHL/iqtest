10c43fbb67bcdc73e45ac9f2c302602e3b808e23
ducna <ducnguyen19971312@gmail.com>
2024-07-19 03:08:32 +0700

    update code

--- iqtest/src/main/java/com/study/iqtest/service/EmailService.java
+++ iqtest/src/main/java/com/study/iqtest/service/EmailService.java
@@ -2,6 +2,7 @@ package com.study.iqtest.service;
 
 import com.study.iqtest.exception.EmailSendingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,17 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendMessage(String to, String subject, String text) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
             message.setTo(to);
             message.setSubject(subject);
             message.setText(text);
+
             emailSender.send(message);
             System.out.println("Email sent successfully to " + to);
         } catch (Exception e) {
