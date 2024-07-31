6c88a3ea6bad13414ce92586b9443b5cc8a6766d
ducna <ducnguyen19971312@gmail.com>
2024-07-31 22:11:54 +0700

    update code

--- iqtest/src/main/java/com/study/iqtest/config/SwaggerConfig.java
+++ iqtest/src/main/java/com/study/iqtest/config/SwaggerConfig.java
@@ -1,33 +1,48 @@
 package com.study.iqtest.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "bearerAuth",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT"
+        )
+})
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth"))
+                .info(new Info().title("IQ Test API")
+                        .version("v0.0.1").description("IQ Test application")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("IQTest-public")
-                .pathsToMatch("/public/**")
+                .pathsToMatch("/api/**")
                 .build();
-    }
-
-    @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("IQ Test API")
-                        .description("IQ Test application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("IQTest Wiki Documentation")
-                        .url("https://spring.io/projects/spring-boot"));
     }
 }
